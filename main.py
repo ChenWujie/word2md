1e43d23d40dce554336844e5ee43ab27efa24e66
chenwujie <1454857834@qq.com>
2023-05-03 22:58:18 +0800

    优化功能和UI

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class MyWin(QDialog):
         string = self.lineEdit.text()
         self.label_2.setText(string)
         self.lineEdit.clear()
-        do(string)      # 网络请求异步执行，否则会闪退
+        if len(string) > 1:
+            do(string)      # 网络请求异步执行，否则会闪退
 
 
 def Print(string):
@@ -40,13 +41,16 @@ def write_func(text):
     k = re.compile('@.+?。')
     today = datetime.date.today().strftime('%y%m%d')
     result = ''
-    with open('./word_files/' + today + '.md', 'a+', encoding='utf-8') as f:
+
+    with open('./word_files/' + today + '_memorize.md', 'a+', encoding='utf-8') as f1, \
+            open('./word_files/' + today + '_review.md', 'a+', encoding='utf-8') as f2:
         if text == 'new':
-            f.write('# {}\n'.format(today))
-        else:
+            f1.write('# {} memorize\n'.format(today))
+            f2.write('# {} review\n'.format(today))
+        elif '@' in text:
             if not text.endswith("。"):
                 text += "。"
-            string = '**' + text.split(' ')[0] + '** '
+            string = '**' + text[:text.find('@')].strip() + '** '
             response = requests.get('https://cn.bing.com/dict/search?q={0}'.format(text.split(' ')[0]))
             us, en = '', ''
             if response.ok:
@@ -55,13 +59,25 @@ def write_func(text):
                 us = soup.find(class_='hd_prUS b_primtxt').text
                 en = soup.find(class_='hd_pr b_primtxt').text
             mean = []
+            part_of_speech = ''
             for i in re.finditer(k, text):
                 temp = i.group()[1: -1].split(' ')
                 mean.append('*' + temp[0] + '.* ')
+                part_of_speech += '*' + temp[0] + '.* ···  '
                 mean.append(temp[1])
             all_mean = ' '.join(mean)
-            result = string + en + ' ' + us + ' ' + all_mean + '\n'
-            f.write(result)
+            mem_result = string + en + ' ' + us + ' ' + all_mean + '\n\n'
+            rev_result = string + en + ' ' + us + ' ' + part_of_speech + '\n\n'
+            mem_result = mem_result.replace('（', '(')
+            mem_result = mem_result.replace('）', ')')
+            rev_result = rev_result.replace('（', '(')
+            rev_result = rev_result.replace('）', ')')
+            mem_result = mem_result.replace('【', '[')
+            mem_result = mem_result.replace('】', ']')
+            rev_result = rev_result.replace('【', '[')
+            rev_result = rev_result.replace('】', ']')
+            f1.write(mem_result)
+            f2.write(rev_result)
 
 
 def do(text):
